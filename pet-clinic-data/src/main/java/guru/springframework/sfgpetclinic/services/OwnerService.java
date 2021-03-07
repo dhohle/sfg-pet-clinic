bacfd0ae6e7b74c5b05e3b32f2ce6fc0cc31d15b
Daniël Höhle <daniel.hohle@gmail.com>
2021-03-07 08:21:35 +0100

    Added bootstrap for data. Closes #16

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
@@ -2,6 +2,7 @@ package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Owner;
 
+import java.util.List;
 import java.util.Set;
 
 public interface OwnerService extends CrudService<Owner, Long>{
@@ -9,4 +10,5 @@ public interface OwnerService extends CrudService<Owner, Long>{
     Owner findByLastName(final String lastName);
 
 
+    List<Owner> findAllByLastNameLike(String lastName);
 }
