35226e3a916b1ae96bdcfb89d0f74539c3c016b5
Daniël Höhle <daniel.hohle@gmail.com>
2021-03-08 16:02:27 +0100

    Convert Owners to JPA Entities. Closes #39

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -1,8 +1,15 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+// @MappedSuperClass tells that this class will not be mapped directly to the database, but is used by other classes
+@MappedSuperclass
 public class Person extends BaseEntity {
 
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
 
 
