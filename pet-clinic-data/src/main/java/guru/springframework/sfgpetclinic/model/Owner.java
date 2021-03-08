1c86213520d1fd9a057e19de8a50f1e59ab4a973
Daniël Höhle <daniel.hohle@gmail.com>
2021-03-08 09:04:31 +0100

    Create PetType Map Service Impl. Closes #32

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -5,8 +5,8 @@ import java.util.Set;
 public class Owner extends Person {
 
     private String address;
-    private  String city;
-    private  String telephone;
+    private String city;
+    private String telephone;
 
     private Set<Pet> pets;
 
